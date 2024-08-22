34ff7ef2cd724d3130fd535642f44014cbe635f5
mdrennen87 <mdrennen87@gmail.com>
2024-08-21 19:46:30 -0700

    Added event listeners for button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 var button = document.querySelector(".show-cat");
-// console.log(button);
-
 var cat = document.querySelector(".cat");
-// console.log(cat);
 
 button.addEventListener("click", function () {
-    // button.innerText = "It's working.";
-    cat.classList.add("show");
-});
+   if (cat.classList.contains("show")) {
+    // console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+   } else {
+    // console.log("No");
+    cat.classList.add("show"); 
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+   }
+}); 
